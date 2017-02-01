63d1e02a4e736254d54e922ebf8b90cbf97f6df7
LOSKARTOFLOSPC\LosKartoflos <basti@gritzbach.eu>
2017-02-01 12:50:24 +0100

    movement.movement chekcer doesn't work anymore.  error at line 105(103 without debug.log)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,16 +7,14 @@ public class Movement : MonoBehaviour {
 
     //settings
     public float originalSpeed = 5f;
+    public string direction; //to do: make this better(protected)
     private float speed;
+
 
     //save the current and the last tile standing on
     SetupTile tileScriptCurrent;
     SetupTile tileScriptOld;
     SetupTile activeTile;
-
-    //allowed walking directions
-    //delete private bool left = false, right = false, forward = false, back = false, oldLeft = false, oldRight = false, oldForward = false, oldBack = false;
-
 
     private void Start()
     {
@@ -25,25 +23,13 @@ public class Movement : MonoBehaviour {
 
     void Update () {
 
-        //input/movement part
-        if (Input.GetKey("w") )
-        {
-            moveFigure(checkIfWalkdirectionIsAllowed("forward", tileScriptCurrent, tileScriptOld));
-        }
-        if (Input.GetKey("s") )
-        {
-            moveFigure(checkIfWalkdirectionIsAllowed("back", tileScriptCurrent, tileScriptOld));
-        }
-        if (Input.GetKey("a"))
-        {
-            moveFigure(checkIfWalkdirectionIsAllowed("left", tileScriptCurrent, tileScriptOld));
-        }
-        if (Input.GetKey("d") )
-        {
-            moveFigure(checkIfWalkdirectionIsAllowed("right", tileScriptCurrent, tileScriptOld));
-        }
+        //movementChecker returns if its possible the wished move direction. Makes little Corrections at corners
+        moveFigure(movementChecker(direction, tileScriptCurrent, tileScriptOld));
+       
     }
 
+
+    //moves the Gameobject
     private void moveFigure(string _direction)
     {
         if (_direction == "forward")
@@ -63,6 +49,8 @@ public class Movement : MonoBehaviour {
         // the next current and old tile need to be in a line, this is checked by the leaste indented if clauses (or else it's a corner)
         // if the old and the next tile are 2 apart, then there is no direction change.
 
+
+        Debug.Log(tileScriptNext.name);
         //inital
         if(tileScriptCurrent == null)
         {
@@ -105,91 +93,15 @@ public class Movement : MonoBehaviour {
             tileScriptCurrent = tileScriptNext;
         }
         
-
-        //Delete setAllowedDirections(tileScript);
         //Delete Debug.Log("Tile: " + tileScript.name +"\nL: " + tileScript.left + "; R: " + tileScript.right + "; F: " + tileScript.forward + "; B: " + tileScript.back + ";    " + left + ", " + right + ", " + forward + ", " + back);
 
     }
 
 
     //Enter thw wished directions and the current and old tile, a bool value will be given if direction is allowed (at corners it will guide the figure to the right z or x position (the middle of the tile)
-    private string checkIfWalkdirectionIsAllowed( string _direction ,SetupTile _tileScriptCurrent, SetupTile _tileSriptOld)
+    private string movementChecker( string _direction ,SetupTile _tileScriptCurrent, SetupTile _tileSriptOld)
     {
-        /*delete
-        //straight vertical
-        if(_tileScriptCurrent.straighthor && _tileSriptOld.straighthor)
-        {          
-            if (direction == "left")
-                return true;
-            else if (direction == "right")
-                return true;
-
-            return false;
-        } //straight horizontal
-        else if (_tileScriptCurrent.straightvert && _tileSriptOld.straightvert)
-        {
-            if (direction == "forward" )
-                return true;
-            else if (direction == "back" )
-                return true;
-
-            return false;
-        }//corner to straight
-        //if (!tileScriptCurrent.straight && )
-        */
-
-       // if the gameobject is at the same height for horizontal or same width for vertical movement and the direction is set true, then true is returned (direction is allowed)
-       // if the new is a corner also the other axis have to be checker
-       // to do: (think about the straight marking again)
-       /*
-        if(direction == "left")
-        {
-            //if current is corner
-            if (tileScriptCurrent.transform.position.z == transform.position.z && tileScriptCurrent.transform.position.x < transform.position.x && !tileScriptCurrent.left && tileScriptOld.left)
-                return true;
-            //on current
-            else if (tileScriptCurrent.transform.position.z == transform.position.z && tileScriptCurrent.left)
-                return true;
-            //on old
-            else if (tileScriptOld.transform.position.z == transform.position.z && tileScriptOld.left);
-            // return true;
-            else
-                return false;
-        }
-
-        else if (direction == "right")
-        {
-            //if current is corner
-            if (tileScriptCurrent.transform.position.z == transform.position.z && tileScriptCurrent.transform.position.x > transform.position.x && !tileScriptCurrent.right && tileScriptOld.right)
-                return true;
-            //on current
-            else if (tileScriptCurrent.transform.position.z == transform.position.z && tileScriptCurrent.right)
-                return true;
-            //on old
-            else if (tileScriptOld.transform.position.z == transform.position.z && tileScriptOld.right);
-            // return true;
-            else
-                return false;
-        }
-
-        else if (direction == "forward")
-        {
-            //if current is corner
-            if (tileScriptCurrent.transform.position.x == transform.position.x && tileScriptCurrent.transform.position.z > transform.position.z && !tileScriptCurrent.forward && tileScriptOld.forward)
-                return true;
-            //on current
-            else if (tileScriptCurrent.transform.position.x == transform.position.x && tileScriptCurrent.forward)
-                return true;
-            //on old
-            else if (tileScriptOld.transform.position.x == transform.position.x && tileScriptOld.forward) ;
-            // return true;
-            else
-                return false;
-        }
-        */
-
-
-        //Select 
+        
         if(tileScriptCurrent.transform.position.z == tileScriptOld.transform.position.z)
         {
             if (Math.Abs(tileScriptCurrent.transform.position.x - transform.position.x) <= 0.5f)
@@ -323,98 +235,8 @@ public class Movement : MonoBehaviour {
                 transform.position = new Vector3(transform.position.x, transform.position.y, activeTile.transform.position.z);
         }
 
-
-
-
-        //straight
-        /*
-
-        else if (_direction == "forward" && activeTile.forward)
-            return _direction;
-        else if (_direction == "back" && activeTile.back)
-            return _direction;
-
-        */
-
-        
-        //right
-       
-        //forward
-        
-        //back
-        
-
-
-
-
         return null;
 
-
     } 
-
-    // set the allowed walking directions depending on the last tile entered und the one before;
-    /*deleteprivate void setAllowedDirections(SetupTile _tileScript)
-    {
-
-
-       
-        //left
-        if (_tileScript.left == true)
-        {
-            oldLeft = left;
-            left = true;
-        }
-        else
-        {
-            oldLeft = left;
-            left = false;
-        }
-        //right
-        if (_tileScript.right == true)
-        {
-            oldRight = right;
-            right = true;
-        }
-        else
-        {
-            oldRight = right;
-            right = false;
-        }
-        //foward
-        if (_tileScript.forward == true)
-        {
-            oldForward = forward;
-            forward = true;
-        }
-        else
-        {
-            oldRight = right;
-            right = false;
-        }
-        //back
-        if (_tileScript.back == true)
-        {
-            oldBack = back;
-            back = true;
-        }
-        else
-        {
-            oldBack = back;
-            back = false;
-        }
-
-    }
-
-
-   private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.tag == "sidewalk")
-        {
-            foreach (GameObject gObject in currentCollisions)
-            {
-                print(gObject.name);
-            }
-        }
-    }*/
 
 }
